d56923f21876294734d18df48b62e24178bd3f11
Eduardo Capella <edufeq03@gmail.com>
2025-09-18 22:32:09 -0300

    Ajustando SKD do projeto para 54 e variáveis de ambiente

--- config/api_config.js
+++ config/api_config.js
@@ -1,4 +1,3 @@
-import Constants from 'expo-constants';
-import { GOOGLE_VISION_API_KEY as ENV_GOOGLE_VISION_API_KEY } from '@env';
+import { REACT_APP_GOOGLE_VISION_API_KEY } from '@env';
 
-export const GOOGLE_CLOUD_VISION_API_KEY = ENV_GOOGLE_VISION_API_KEY || Constants.expoConfig.extra.GOOGLE_VISION_API_KEY;
+export const GOOGLE_CLOUD_VISION_API_KEY = REACT_APP_GOOGLE_VISION_API_KEY;
--- config/firebase_config.js
+++ config/firebase_config.js
@@ -4,28 +4,26 @@ import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 import { initializeAuth, getReactNativePersistence } from 'firebase/auth';
 import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage';
-import Constants from 'expo-constants';
 import {
-    FIREBASE_API_KEY as ENV_FIREBASE_API_KEY,
-    FIREBASE_AUTH_DOMAIN as ENV_FIREBASE_AUTH_DOMAIN,
-    FIREBASE_PROJECT_ID as ENV_FIREBASE_PROJECT_ID,
-    FIREBASE_STORAGE_BUCKET as ENV_FIREBASE_STORAGE_BUCKET,
-    FIREBASE_MESSAGING_SENDER_ID as ENV_FIREBASE_MESSAGING_SENDER_ID,
-    FIREBASE_APP_ID as ENV_FIREBASE_APP_ID,
-    FIREBASE_MEASUREMENT_ID as ENV_FIREBASE_MEASUREMENT_ID,
+    REACT_APP_FIREBASE_API_KEY,
+    REACT_APP_FIREBASE_AUTH_DOMAIN,
+    REACT_APP_FIREBASE_PROJECT_ID,
+    REACT_APP_FIREBASE_STORAGE_BUCKET,
+    REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    REACT_APP_FIREBASE_APP_ID,
+    REACT_APP_FIREBASE_MEASUREMENT_ID,
 } from '@env';
 
 const firebaseConfig = {
-    apiKey: ENV_FIREBASE_API_KEY || Constants.expoConfig.extra.FIREBASE_API_KEY,
-    authDomain: ENV_FIREBASE_AUTH_DOMAIN || Constants.expoConfig.extra.FIREBASE_AUTH_DOMAIN,
-    projectId: ENV_FIREBASE_PROJECT_ID || Constants.expoConfig.extra.FIREBASE_PROJECT_ID,
-    storageBucket: ENV_FIREBASE_STORAGE_BUCKET || Constants.expoConfig.extra.FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: ENV_FIREBASE_MESSAGING_SENDER_ID || Constants.expoConfig.extra.FIREBASE_MESSAGING_SENDER_ID,
-    appId: ENV_FIREBASE_APP_ID || Constants.expoConfig.extra.FIREBASE_APP_ID,
-    measurementId: ENV_FIREBASE_MEASUREMENT_ID || Constants.expoConfig.extra.FIREBASE_MEASUREMENT_ID
+    apiKey: REACT_APP_FIREBASE_API_KEY,
+    authDomain: REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: REACT_APP_FIREBASE_APP_ID,
+    measurementId: REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
-// Verifica se o Firebase já foi inicializado
 const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApp();
 
 export const db = getFirestore(app);
